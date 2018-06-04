77c775fc79e70ae2e9286ed2883fb351d00d4b66
Mike Chang <mchang0926@gmail.com>
2018-06-03 20:54:54 -0700

    Create Stress_reliever.py

---
+++ Stress_reliever.py
@@ -0,0 +1,18 @@
+# Stress_reliever.py 
+# 
+# 
+
+# ask the user for answer
+a = input("\nDo you have a problem in life (yes/no)? ")
+
+if (a == "No") or (a == "no") or (a == "n"):
+  # Display the result
+  print("\nThen why worry?")
+
+elif (a == "Yes") or (a == "yes") or (a == "y"):
+
+  b = input("\nCan you do something about it? ")
+
+  if b == "yes" or b == "y" or b == "no" or b == "n":
+
+    print("\nThen why worry?")
