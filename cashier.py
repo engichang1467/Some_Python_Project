693720ba8c28c7eb7064284d7d149e9c60919f81
Mike Chang <mchang0926@gmail.com>
2018-08-26 12:31:21 -0700

    Create cashier.py

---
+++ cashier.py
@@ -0,0 +1,312 @@
+#cashier.py
+def start():
+    print("Welcome to the KIN'S FARM cashier training program")
+    a = input("Please type in the code:")
+
+    if a =="11":
+        print("")
+        print("Gala Apple")
+        print("")
+
+    elif a=="15":
+        print("")
+        print("Macintoch Apple")
+        print("")
+
+    elif a=="16":
+        print("")
+        print("Local Spartan Apple")
+        print("")
+
+    elif a=="27":
+        print("")
+        print("Honey Crisp Apple")
+        print("")
+
+    elif a=="31":
+        print("")
+        print("Envy Apple")
+        print("")
+
+    elif a=="47":
+        print("")
+        print("Raspberry")
+        print("")
+
+    elif a=="50":
+        print("")
+        print("Blueberry")
+        print("")
+
+    elif a=="56":
+        print("")
+        print("Strawberry")
+        print("")
+
+    elif a=="62":
+        print("")
+        print("Blackberry")
+        print("")
+
+    elif a=="70":
+        print("")
+        print("Mandarin")
+        print("")
+
+    elif a=="102":
+        print("")
+        print("Green Grape")
+        print("")
+
+    elif a=="105":
+        print("")
+        print("Coronation Grape")
+        print("")
+
+    elif a=="107":
+        print("")
+        print("Green Natural Thompson Grape")
+        print("")
+
+    elif a=="113":
+        print("")
+        print("Red Sweet Celebration Grape")
+        print("")
+
+    elif a=="151":
+        print("")
+        print("Tangerine Honey")
+        print("")
+
+    elif a=="160":
+        print("")
+        print("Grapefruit")
+        print("")
+
+    elif a=="173":
+        print("")
+        print("Korean Melon")
+        print("")
+
+    elif a=="186":
+        print("")
+        print("Hami Melon")
+        print("")
+
+    elif a=="191":
+        print("")
+        print("Cantaloupe Melon")
+        print("")
+
+    elif a=="192":
+        print("")
+        print("Cut Cantaloupe Melon")
+        print("")
+
+    elif a=="193":
+        print("")
+        print("Honeydew Melon")
+        print("")
+
+    elif a=="193":
+        print("")
+        print("Cut Honeydew Melon")
+        print("")
+
+    elif a=="197":
+        print("")
+        print("Seedless watermelon")
+        print("")
+
+    elif a=="198":
+        print("")
+        print("Cut Seedless watermelon")
+        print("")
+
+    elif a=="199":
+        print("")
+        print("Mini watermelon")
+        print("")
+
+    elif a=="202":
+        print("")
+        print("Jet Mango")
+        print("")
+
+    elif a=="209":
+        print("")
+        print("Jumbo Avocado")
+        print("")
+
+    elif a=="210":
+        print("")
+        print("Avocado")
+        print("")
+
+    elif a=="214":
+        print("")
+        print("Green Kiwi")
+        print("")
+
+    elif a=="215":
+        print("")
+        print("Yellow Kiwi")
+        print("")
+
+    elif a=="218":
+        print("")
+        print("Hawaii Papaya")
+        print("")
+
+    elif a=="219":
+        print("")
+        print("Maradol Papaya")
+        print("")
+
+    elif a=="223":
+        print("")
+        print("Coconut")
+        print("")
+
+    elif a=="226":
+        print("")
+        print("Star Fruit")
+        print("")
+
+    elif a=="231":
+        print("")
+        print("Mango")
+        print("")
+
+    elif a=="238":
+        print("")
+        print("Guava")
+        print("")
+
+    elif a=="241":
+        print("")
+        print("Bartlett Pear")
+        print("")
+
+    elif a=="249":
+        print("")
+        print("Fragrant Pear")
+        print("")
+
+    elif a=="272":
+        print("")
+        print("Black Plum")
+        print("")
+
+    elif a=="278":
+        print("")
+        print("Prume Plum")
+        print("")
+
+    elif a=="279":
+        print("")
+        print("Prume President Local Plum")
+        print("")
+
+    elif a=="282":
+        print("")
+        print("Pluot Plum")
+        print("")
+
+    elif a=="301":
+        print("")
+        print("White Necterine")
+        print("")
+
+    elif a=="306":
+        print("")
+        print("Yellow Peach")
+        print("")
+
+    elif a=="307":
+        print("")
+        print("White Peach")
+        print("")
+
+    elif a=="311":
+        print("")
+        print("Donut Peach")
+        print("")
+
+    elif a=="336":
+        print("")
+        print("lemon")
+        print("")
+
+    elif a=="338":
+        print("")
+        print("lime")
+        print("")
+
+    elif a=="501":
+        print("")
+        print("Green Bean")
+        print("")
+
+    elif a=="532":
+        print("")
+        print("English Long Cucumber")
+        print("")
+
+    elif a=="533":
+        print("")
+        print("Field Cucumber")
+        print("")
+
+    elif a=="536":
+        print("")
+        print("Japanese Cucumber")
+        print("")
+
+    elif a=="537":
+        print("")
+        print("Mini Cucumber")
+        print("")
+
+    elif a=="551":
+        print("")
+        print("Local Cauliflower")
+        print("")
+
+    elif a=="553":
+        print("")
+        print("Cello Cauliflower")
+        print("")
+
+    elif a=="560":
+        print("")
+        print("Eggplant")
+        print("")
+
+    elif a=="561":
+        print("")
+        print("Long Eggplant")
+        print("")
+
+    elif a=="642":
+        print("")
+        print("Green Onion")
+        print("")
+
+    elif a=="641":
+        print("")
+        print("Cooking Onion")
+        print("")
+
+    elif a=="643":
+        print("")
+        print("Onion (Leeks)")
+        print("")
+
+    else:
+        print("")
+        print("I don't know what this code is")
+        print("")
+    return start()
+
+start()
