8ce05f0266439756d6de371b7a3d92564e61cde8
Mike Chang <mchang0926@gmail.com>
2018-08-31 19:19:55 -0700

    Create cashier2.py
    This is the improvement of my previous project "cashier.py". It'll search through the textfile, and find the fruit from the number that the user input.

---
+++ Cashier2.0/cashier2.py
@@ -0,0 +1,32 @@
+
+def Cashier2():
+
+	think = True
+
+	while(think):
+
+		fileR = open("fruit-code.txt", "r")
+		line = fileR.readlines()
+
+    # Ask user for input
+		n = input("Write in the code (type Q to quit): ")
+
+    # this will split the list from newline (\n)
+		for l in line:
+			a = l.split(" ")  # and split it again from space
+      
+      # This will search the number that the user input
+			if (n == a[0]):
+      
+        # As it found the fruit that match the number, it'll display the name of the fruit
+				print("\n{}".format(a[1]))
+		
+    # This will allow the user to leave the program
+		if n == "q":
+			think = False
+			print("\nGood bye\n")		
+
+		fileR.close()
+
+
+Cashier2()
