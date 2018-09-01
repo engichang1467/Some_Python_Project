67e44159a598b07ffa030490db5e72d8587264dc
Mike Chang <mchang0926@gmail.com>
2018-08-31 19:21:49 -0700

    Update cashier2.py

--- Cashier2.0/cashier2.py
+++ Cashier2.0/cashier2.py
@@ -8,20 +8,20 @@ def Cashier2():
 		fileR = open("fruit-code.txt", "r")
 		line = fileR.readlines()
 
-    # Ask user for input
+    		# Ask user for input
 		n = input("Write in the code (type Q to quit): ")
 
-    # this will split the list from newline (\n)
+    		# this will split the list from newline (\n)
 		for l in line:
 			a = l.split(" ")  # and split it again from space
       
-      # This will search the number that the user input
+      			# This will search the number that the user input
 			if (n == a[0]):
       
-        # As it found the fruit that match the number, it'll display the name of the fruit
+        			# As it found the fruit that match the number, it'll display the name of the fruit
 				print("\n{}".format(a[1]))
 		
-    # This will allow the user to leave the program
+    		# This will allow the user to leave the program
 		if n == "q":
 			think = False
 			print("\nGood bye\n")		
